936df146116181c223d8397c7df148f7606c8672
sync-cmd <1998aadityaraj2007@gmail.com>
2026-07-06 15:11:44 +0530

    Enhance menu interaction with nav item click events
    Added event listeners to nav items to close menu on click.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li a');
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 };
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
